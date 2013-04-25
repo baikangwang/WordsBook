9e1f2bdb2665fa35601aff529f00fcdc056b0038
Baker Wang <baikangwang@hotmail.com>
2013-04-25 15:59:00 +0800

    Initial Check in

---
+++ WordsBook/WordsBook.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace WordsBook
+{
+    partial class WordsBook : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public WordsBook()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.btnRecite = this.Factory.CreateRibbonToggleButton();
+            this.tab1.SuspendLayout();
+            this.group1.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.Groups.Add(this.group1);
+            this.tab1.Label = "TabAddIns";
+            this.tab1.Name = "tab1";
+            // 
+            // group1
+            // 
+            this.group1.Items.Add(this.btnRecite);
+            this.group1.Label = "WordsBook";
+            this.group1.Name = "group1";
+            // 
+            // btnRecite
+            // 
+            this.btnRecite.Label = "Recite";
+            this.btnRecite.Name = "btnRecite";
+            this.btnRecite.ShowImage = true;
+            this.btnRecite.ShowLabel = false;
+            this.btnRecite.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btnRecite_Click);
+            // 
+            // WordsBook
+            // 
+            this.Name = "WordsBook";
+            this.RibbonType = "Microsoft.Excel.Workbook";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.WordsBook_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.group1.ResumeLayout(false);
+            this.group1.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonToggleButton btnRecite;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal WordsBook WordsBook
+        {
+            get { return this.GetRibbon<WordsBook>(); }
+        }
+    }
+}
---
+++ WordsBook/WordsBook.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace WordsBook
+{
+    public partial class WordsBook
+    {
+        private void WordsBook_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void btnRecite_Click(object sender, RibbonControlEventArgs e)
+        {
+            Excel.Window window = e.Control.Context;
+            Excel.Worksheet activeWorksheet = ((Excel.Worksheet)window.Application.ActiveSheet);
+            Excel.Range firstRow = activeWorksheet.get_Range("A1");
+            firstRow.EntireColumn.Hidden = btnRecite.Checked;
+        }
+    }
+}
